5772203fd2f5de9eb2a18fd57fc7caa35ce26bef
Harley Rogers <harleydev512@gmail.com>
2020-06-17 14:44:50 -0500

    This is some C# code. Initial project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a new line made in the Comand Console!");
+            Console.WriteLine("Command was spelt wrong. Oops!");
+        }
+    }
+}
